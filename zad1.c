45ea261cecdc338dd9a6bba482df313416cd1a6e
IsaVasil <105173734+IsaVasil@users.noreply.github.com>
2022-07-19 17:07:15 +0300

    Add files via upload

--- zad1.c
+++ zad1.c
@@ -3,11 +3,11 @@
 #include "ramdoms.h"
 #define count 10
 
-double compareint1(const void*num1,const void*num2){
+int comparient1(const void*num1,const void*num2){
 return *(double*)num1-*(double*)num2;
 
 }
-double compareint2(const void*num1,const void*num2){
+int compareint2(const void*num1,const void*num2){
 return *(double*)num2-*(double*)num1;
 
 }
@@ -15,7 +15,7 @@ return *(double*)num2-*(double*)num1;
 int main(){
     int num;
      scanf("%d",&num);
-double(*calcFunc[2])(const void*,const void*)= {comparient1,compareint2};
+int(*calcFUnction[2])(const void*,const void*)= {comparient1,compareint2};
 //int (*myComparator) (const void*,const void*);
 //myComparator =&compareint2;
 
